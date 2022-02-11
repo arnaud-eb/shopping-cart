2f232137c0f6ac1a79f3581dff1a9f5a45e6db4b
arnaud-eb <arnaud.depierreux@easebest.com>
2022-02-11 13:54:18 +0100

    Refactor App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
 
-function App() {
+import Navbar from "./components/Navbar";
+import Cart from "./components/Cart";
+
+import { useCartContext } from "./context";
+
+const Loading = styled.h1`
+  text-align: center;
+  margin-top: 5rem;
+`;
+
+const App = () => {
+  const { loading } = useCartContext();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      {loading ? (
+        <Loading>loading...</Loading>
+      ) : (
+        <React.Fragment>
+          <Navbar />
+          <Cart />
+        </React.Fragment>
+      )}
+    </main>
   );
-}
+};
 
 export default App;
